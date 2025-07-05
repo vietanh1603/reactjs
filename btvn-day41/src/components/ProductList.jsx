0ecc6732dc472c212dc0a9b0e4915757b88ecde3
vietanh <anhvhf8@fullstack.edu.vn>
2025-07-05 16:07:44 +0700

    bai tap ve nha buoi 41

--- btvn-day41/src/components/ProductList.jsx
+++ btvn-day41/src/components/ProductList.jsx
@@ -1,10 +1,14 @@
 
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteProduct, updateProduct } from '../store/actions';
+import { TextField } from '@mui/material';
+import {useState} from "react";
+
 
 export default function ProductList() {
     const products = useSelector(state => state.products);
     const dispatch = useDispatch();
+    const [searchId, setSearchId] = useState('');
 
     const handleEdit = (product) => {
         const name = prompt('Tên mới:', product.name);
@@ -19,34 +23,46 @@ export default function ProductList() {
             unit
         }));
     };
-
+    const filteredProducts = searchId
+        ? products.filter(p => p.id.toLowerCase().includes(searchId.toLowerCase()))
+        : products;
     return (
-        <table border="1" cellPadding="8" cellSpacing="0">
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Tên sản phẩm</th>
-                <th>Giá bán (VNĐ)</th>
-                <th>Số lượng</th>
-                <th>Đơn vị</th>
-                <th>Hành động</th>
-            </tr>
-            </thead>
-            <tbody>
-            {products.map(product => (
-                <tr key={product.id}>
-                    <td>{product.id}</td>
-                    <td>{product.name}</td>
-                    <td>{product.price.toLocaleString()}</td>
-                    <td>{product.quantity}</td>
-                    <td>{product.unit}</td>
-                    <td>
-                        <button onClick={() => handleEdit(product)}>Sửa</button>
-                        <button onClick={() => dispatch(deleteProduct(product.id))}>Xoá</button>
-                    </td>
+        <div>
+            <TextField
+                type="text"
+                label="Search ID"
+                value={searchId}
+                onChange={(e) => setSearchId(e.target.value)}
+                style={{ marginBottom: '20px'}}
+            />
+
+            <table border="1" cellPadding="8" cellSpacing="0">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Tên sản phẩm</th>
+                    <th>Giá bán (VNĐ)</th>
+                    <th>Số lượng</th>
+                    <th>Đơn vị</th>
+                    <th>Hành động</th>
                 </tr>
-            ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                {filteredProducts.map(product => (
+                    <tr key={product.id}>
+                        <td>{product.id}</td>
+                        <td>{product.name}</td>
+                        <td>{product.price.toLocaleString()}</td>
+                        <td>{product.quantity}</td>
+                        <td>{product.unit}</td>
+                        <td>
+                            <button onClick={() => handleEdit(product)}>Sửa</button>
+                            <button onClick={() => dispatch(deleteProduct(product.id))}>Xoá</button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
